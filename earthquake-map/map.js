5fa9fc7b1fddcf6b074c5f5f97252da7f6db44bd
Ubuntu <ubuntu@ip-172-31-28-104.ap-southeast-1.compute.internal>
2019-07-12 03:57:46 +0000

    Google Map for Earthquakes

--- earthquake-map/map.js
+++ earthquake-map/map.js
@@ -1,30 +1,36 @@
 // global variable
 let map;
 let endpoint = "https://earthquake.usgs.gov/fdsnws/event/1/query";
-
+let params = {
+    'lat':0.7893,
+    'lng': 113.9213,
+    'radius' : 2000
+}
+let markers = [];
 function init() {
     // vanila JavaScript:
     // map = new google.maps.Map(document.getElementById("map"));
 
     // Using jQuery
     let div_map = $("#map")[0];
-    map = new google.maps.Map(div_map, {
+    map = new google.maps.Map(div_map, { 
         'center': { lat: 0.7893, lng: 113.9213 },
         'zoom': 6
     });
 
 }
 
-function getEarthquakeDataAsync(consumer) {
+function getEarthquakeDataAsync(consumer, config) {
+    
     let parameters = {
         'format': 'geojson',
         'starttime': '2014-01',
         'endtime': '2014-02',
-        'longitude': 113.9213,
-        'latitude': 0.7893,
-        'maxradiuskm': 2000
+        'longitude': config.lng,
+        'latitude': config.lat,
+        'maxradiuskm':config.radius
     }
-
+    
     axios.get(endpoint, {
         'params': parameters
     }).then((response) => {
@@ -62,24 +68,58 @@ function consumeEarthquakeData(earthquakes) {
         }
     })
     
+    // remove all earthqaukes with magnitude less than 5
     earthquakes = earthquakes.filter(function(item){
         return item.mag >= 5;
     })
     
+    console.log("---- Before removing markers");
+     console.log(markers);
+   
+    // remove all markers
+     markers.forEach(function(marker){
+        marker.setMap(null)
+    });
+    
+    // Set markers array to be empty
+    markers = [];
+    console.log("After removing markers -->")
+    // show marker for each earthquake
     earthquakes.forEach(showEarthquakes);
+    
+     console.log(markers);
     
     console.log(earthquakes);
     // earthquakes.forEach(showEarthquakes);
+    console.log(params.lat)
+    map.setCenter({
+        lat:parseFloat(params.lat),
+        lng:parseFloat(params.lng)
+    })
 }
 
 let showEarthquakes = (each_earthquake, index) => {
+  
+   
     let marker = new google.maps.Marker({
         'position': { lat: each_earthquake.lat, lng: each_earthquake.lng },
         'map': map,
         'title': each_earthquake.title
     })
+    
+
+    markers.push(marker);
 }
 
 $(function() {
-    getEarthquakeDataAsync(consumeEarthquakeData);
-})
+    getEarthquakeDataAsync(consumeEarthquakeData, params);
+
+    $('#update-btn').click(function(){
+       params.lat = $("#lat").val();
+       params.lng = $('#lng').val();
+       params.radius = $("#radius").val();
+       params.mag = $("#mag").val();
+       getEarthquakeDataAsync(consumeEarthquakeData, params);
+    })
+    
+});
